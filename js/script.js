f041794119d6ff7072aca6f5423541bbe29f86c2
Didas Mbalanya <didasmbalanya@gmail.com>
2018-11-30 14:28:20 +0300

    add id tag on html input element

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-var triangleCalculator(function(side1,side2,side3){
+/*var triangleCalculator(function(side1,side2,side3){
 
 	var triangleSides = [sideA,sideB,sideC]
 	if (triangleSides[1] === triangleSides[2] && triangleSides[2] === triangleSides[3]){
@@ -21,7 +21,6 @@ var triangleCalculator(function(side1,side2,side3){
 		window.alert("Cannot form a triangle")
 	}
 
-		});
+		});*/
+		document.getElementsById("demo").innerHTML = "hello dolly";
 
-function triangleCalculator() {
-    document.getElementById("calc").submit();
