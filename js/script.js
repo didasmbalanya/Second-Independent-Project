f89444e5fbafc7b75aa56ef5d3794b21506ad1a7
Didas Mbalanya <didasmbalanya@gmail.com>
2018-11-30 12:09:21 +0300

    add id to form element for Javascript reference

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-sideA = prompt("Enter side of 1 of triangle: ")
-sideB = prompt("Enter side of 2 of triangle: ")
-sideC = prompt("Enter side of 3 of triangle: ")
 
-var triangleSides = [sideA,sideB,sideC]
+var triangleCalculator(function(side1,side2,side3){
 
-if (triangleSides[1] === triangleSides[2] && triangleSides[2] === triangleSides[3]){
-	console.log("Triangle is Equilateral")
-}
-else if (triangleSides[1] === triangleSides[2] && triangleSides[2] !== triangleSides[3]){
-	console.log("Triangle is Isoceles")
-}
-else if (triangleSides[1] !== triangleSides[2] && triangleSides[2] === triangleSides[3]){
-	console.log("Triangle is Isoceles")
-}
-else if (triangleSides[1] === triangleSides[3] && triangleSides[2] !== triangleSides[3]){
-	console.log("Triangle is Isoceles")
-}
-else{
-	console.log("Triangle is Scalene")
-}
+	var triangleSides = [sideA,sideB,sideC]
+	if (triangleSides[1] === triangleSides[2] && triangleSides[2] === triangleSides[3]){
+		console.log("Triangle is Equilateral")
+	}
+	else if (triangleSides[1] === triangleSides[2] && triangleSides[2] !== triangleSides[3]){
+		console.log("Triangle is Isoceles")
+	}
+	else if (triangleSides[1] !== triangleSides[2] && triangleSides[2] === triangleSides[3]){
+		console.log("Triangle is Isoceles")
+	}
+	else if (triangleSides[1] === triangleSides[3] && triangleSides[2] !== triangleSides[3]){
+		console.log("Triangle is Isoceles")
+	}
+	else{
+		console.log("Triangle is Scalene")
+	}});
+
+function triangleCalculator() {
+    document.getElementById("frm1").submit();
