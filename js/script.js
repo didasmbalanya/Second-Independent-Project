ae8e3de5ec4cf757653fe36b387a3cc1418b72fb
Didas Mbalanya <didasmbalanya@gmail.com>
2018-11-30 12:42:28 +0300

    add link from html to js file

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@ var triangleCalculator(function(side1,side2,side3){
 
 	var triangleSides = [sideA,sideB,sideC]
 	if (triangleSides[1] === triangleSides[2] && triangleSides[2] === triangleSides[3]){
-		console.log("Triangle is Equilateral")
+		alert("Triangle is Equilateral")
 	}
 	else if (triangleSides[1] === triangleSides[2] && triangleSides[2] !== triangleSides[3]){
-		console.log("Triangle is Isoceles")
+		alert("Triangle is Isoceles")
 	}
 	else if (triangleSides[1] !== triangleSides[2] && triangleSides[2] === triangleSides[3]){
-		console.log("Triangle is Isoceles")
+		alert("Triangle is Isoceles")
 	}
 	else if (triangleSides[1] === triangleSides[3] && triangleSides[2] !== triangleSides[3]){
-		console.log("Triangle is Isoceles")
+		alert("Triangle is Isoceles")
 	}
 	else if(triangleSides[1] !== triangleSides[3] && triangleSides[2] !== triangleSides[3] && triangleSides[1] !== triangleSides[2]){
-		console.log("Triangle is Isoceles")
+		alert("Triangle is Isoceles")
 	}
 	else{
-		console.log("Cannot form a triangle")
+		alert("Cannot form a triangle")
 	}
 
 		});
