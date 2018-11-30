f7abd3a1514d032bf09b48224c6f35b045a6bac9
Didas Mbalanya <didasmbalanya@gmail.com>
2018-11-30 14:11:04 +0300

    add window.alert syntax

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@ var triangleCalculator(function(side1,side2,side3){
 
 	var triangleSides = [sideA,sideB,sideC]
 	if (triangleSides[1] === triangleSides[2] && triangleSides[2] === triangleSides[3]){
-		alert("Triangle is Equilateral")
+		window.alert("Triangle is Equilateral")
 	}
 	else if (triangleSides[1] === triangleSides[2] && triangleSides[2] !== triangleSides[3]){
-		alert("Triangle is Isoceles")
+		window.alert("Triangle is Isoceles")
 	}
 	else if (triangleSides[1] !== triangleSides[2] && triangleSides[2] === triangleSides[3]){
-		alert("Triangle is Isoceles")
+		window.alert("Triangle is Isoceles")
 	}
 	else if (triangleSides[1] === triangleSides[3] && triangleSides[2] !== triangleSides[3]){
-		alert("Triangle is Isoceles")
+		window.alert("Triangle is Isoceles")
 	}
 	else if(triangleSides[1] !== triangleSides[3] && triangleSides[2] !== triangleSides[3] && triangleSides[1] !== triangleSides[2]){
-		alert("Triangle is Isoceles")
+		window.alert("Triangle is Isoceles")
 	}
 	else{
-		alert("Cannot form a triangle")
+		window.alert("Cannot form a triangle")
 	}
 
 		});
