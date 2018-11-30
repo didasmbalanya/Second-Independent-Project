4deae78f594994cec3603dd59df00c19671fae48
Didas Mbalanya <didasmbalanya@gmail.com>
2018-11-30 15:16:53 +0300

    correct function synax

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-var function triangleCalculator(){ 
+var triangleCalculator = function(){ 
 
 	var sideA = document.getElementById("sideA").value();
 	var sideB = document.getElementById("sideB").value();
@@ -7,22 +7,22 @@ var function triangleCalculator(){
 
 	var triangleSides = [sideA,sideB,sideC]
 	if (triangleSides[1] === triangleSides[2] && triangleSides[2] === triangleSides[3]){
-		window.alert("Triangle is Equilateral")
+		alert("Triangle is Equilateral")
 	}
 	else if (triangleSides[1] === triangleSides[2] && triangleSides[2] !== triangleSides[3]){
-		window.alert("Triangle is Isoceles")
+		alert("Triangle is Isoceles")
 	}
 	else if (triangleSides[1] !== triangleSides[2] && triangleSides[2] === triangleSides[3]){
-		window.alert("Triangle is Isoceles")
+		alert("Triangle is Isoceles")
 	}
 	else if (triangleSides[1] === triangleSides[3] && triangleSides[2] !== triangleSides[3]){
-		window.alert("Triangle is Isoceles")
+		alert("Triangle is Isoceles")
 	}
 	else if(triangleSides[1] !== triangleSides[3] && triangleSides[2] !== triangleSides[3] && triangleSides[1] !== triangleSides[2]){
-		window.alert("Triangle is Isoceles")
+		alert("Triangle is Isoceles")
 	}
 	else{
-		window.alert("Cannot form a triangle")
+		alert("Cannot form a triangle")
 	}
 
 		};
