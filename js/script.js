641cc2fc50092665733a3e504f0d31809607cafd
Didas Mbalanya <didasmbalanya@gmail.com>
2018-11-30 15:31:16 +0300

    correct if syntax for scalene and equi;ateral

--- js/script.js
+++ js/script.js
@@ -5,24 +5,11 @@ var triangleCalculator = function(){
 	var sideB = document.getElementById("sideB").value;
 	var sideC = document.getElementById("sideC").value;
 
-	var triangleSides = [sideA,sideB,sideC]
-	if (triangleSides[1] === triangleSides[2] && triangleSides[2] === triangleSides[3]){
-		alert("Triangle is Equilateral")
+	if (sideA === sideB && sideB === sideC){
+		alert("Equilateral")
 	}
-	else if (triangleSides[1] === triangleSides[2] && triangleSides[2] !== triangleSides[3]){
-		alert("Triangle is Isoceles")
-	}
-	else if (triangleSides[1] !== triangleSides[2] && triangleSides[2] === triangleSides[3]){
-		alert("Triangle is Isoceles")
-	}
-	else if (triangleSides[1] === triangleSides[3] && triangleSides[2] !== triangleSides[3]){
-		alert("Triangle is Isoceles")
-	}
-	else if(triangleSides[1] !== triangleSides[3] && triangleSides[2] !== triangleSides[3] && triangleSides[1] !== triangleSides[2]){
-		alert("Triangle is Isoceles")
-	}
-	else{
-		alert("Cannot form a triangle")
+	else if(sideA !== sideB && sideB !== sideC && sideA !== sideC){
+		alert("Scalene")
 	}
 
 		};
