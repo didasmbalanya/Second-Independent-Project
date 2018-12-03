e1dd751ab16ae7fa905256252253ca4460ac6303
Didas Mbalanya <didasmbalanya@gmail.com>
2018-12-03 09:54:47 +0300

    change if condition on js file

--- js/script.js
+++ js/script.js
@@ -1,31 +1,23 @@
 
 var triangleCalculator = function(){
 
-	var sideA = document.getElementById("sideA").value;
-	var sideB = document.getElementById("sideB").value;
-	var sideC = document.getElementById("sideC").value;
+	var sideA = parseFloat(document.getElementById("sideA").value);
+	var sideB = parseFloat(document.getElementById("sideB").value);
+	var sideC = parseFloat(document.getElementById("sideC").value);
 
-	if(sideA + sideB <= sideC || sideB + sideC <= sideA || sideA + sideC <= sideB){
-		alert("cannot form a triangle")
+	if ((sideA + sideB > sideC) && (sideB + sideC > sideA) && (sideA + sideC > sideB)){
+		if (sideA == sideB && sideB == sideC){
+			alert("Eqiulateral")
+		}
+		else if(sideA == sideB || sideB == sideC || sideA == sideC){
+			alert("Isoceles")
+		}
+		else{
+			alert("scalene")
+		}
 	}
-	else if (sideA === sideB && sideB === sideC){
-		alert("Equilateral")
-	}
-	else if(sideA !== sideB && sideB !== sideC && sideA !== sideC){
-		alert("Scalene")
-	}
-	else if (sideA === sideB && sideB !== sideC){
-		alert("Isoceles")
-	}
-	else if(sideA !== sideB && sideB === sideC){
-		alert("Isoceles")
-	}
-	else if(sideA === sideC && sideC !== sideB){
-		alert("Isoceles")
-	}
-	else {
-		alert("Not a triangle")
-	}
-
-		};
+	else{
+		alert("Cannot form a triangle")
+		}
+	};
 		
